c6263bba1d71ddc9a0e6175adbd67323272e4edd
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-20 16:12:40 +0530

    login model and loggedin state created

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import Note from "./Components/Note";
 import CreateArea from "./Components/CreateArea";
 import { Toaster } from "react-hot-toast";
 import DropdownMenu from "./Components/DropdownMenu";
+import LoginModel from "./Components/login/LoginModel";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [dropDown,setDropdown] = useState(false)
+  const [dropDown, setDropdown] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [loginModel, setLoginModel] = useState(false)
 
   const ref = useRef()
 
@@ -18,11 +21,37 @@ function App() {
         setDropdown(false);
       }
     }
-      document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     }
-  },[ref])
+  }, [ref])
+
+  return (
+    <>
+      <div className="h-screen w-full justify-between">
+        <Header dropDown={dropDown} setDropdown={setDropdown} />
+        <CreateArea onAdd={addNote} />
+        {loginModel && <LoginModel setLoginModel={setLoginModel} setLoggedIn={setLoggedIn} />}
+        <DropdownMenu refVar={ref} DropdownStatus={dropDown} loggedIn={loggedIn} setLoginModel={setLoginModel} />
+        <div className="z-10 grid grid-cols-3 gap-5 justify-center overflow-x-scroll px-[150px] py-[50px] shrink-0">
+          {notes.map((noteItem, index) => {
+            return (
+              <Note
+                key={index}
+                id={index}
+                title={noteItem.title}
+                content={noteItem.content}
+                onDelete={deleteNote}
+              />
+            );
+          })}
+        </div>
+        <Toaster position="bottom-right" />
+        <Footer />
+      </div>
+    </>
+  );
 
   function addNote(newNote) {
     setNotes(prevNotes => {
@@ -37,29 +66,6 @@ function App() {
       });
     });
   }
-
-  return (
-    <div className="h-[100%] w-full justify-between">
-      <Header dropDown={dropDown} setDropdown={setDropdown} />
-      <CreateArea onAdd={addNote} />
-      <DropdownMenu refVar={ref} DropdownStatus={dropDown}/>
-      <div className="z-10 grid grid-cols-3 gap-5 justify-center overflow-x-scroll px-[150px] py-[50px] shrink-0">
-      {notes.map((noteItem, index) => {
-        return (
-          <Note
-            key={index}
-            id={index}
-            title={noteItem.title}
-            content={noteItem.content}
-            onDelete={deleteNote}
-          />
-        );
-      })}
-      </div>
-      <Toaster position="bottom-right" />
-      <Footer />
-    </div>
-  );
 }
 
 export default App;
