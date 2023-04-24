f4224bc7c08250f625be0909d26ae7d7b8db1ce6
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-24 15:13:16 +0530

    query hard value removed

--- src/queries/noteQueries.js
+++ src/queries/noteQueries.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client"
 
 const GET_NOTES = gql`
     query getNotes {
-        Notes(id:"643c09bafa4b04106d925779"){
+        Notes{
           title,
           description
         }
