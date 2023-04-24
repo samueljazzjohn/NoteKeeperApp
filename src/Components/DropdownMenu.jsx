b66d7a51d11284f7531a0fd7c1e14b18d5cda147
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-24 15:12:32 +0530

    query changed

--- src/Components/DropdownMenu.jsx
+++ src/Components/DropdownMenu.jsx
@@ -16,7 +16,7 @@ const DropdownMenu = (props) => {
         <>
             {props.DropdownStatus && <div ref={props.refVar} id="dropdown" className="absolute top-3 md:top-8 right-8 z-90 translate-y-10 bg-white divide-y divide-gray-100 rounded-lg  w-44 border border-gray-500 shadow-lg">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    {!props.loggedIn ? <li><a data-modal-target="defaultModal" data-modal-toggle="defaultModal" onClick={handleLoginModel} href="#" class="block px-4 py-2 text-gray-700 hover:text-gray-500">login</a> </li>
+                    {!localStorage.getItem('isLoggedIn') ? <li><a data-modal-target="defaultModal" data-modal-toggle="defaultModal" onClick={handleLoginModel} href="#" class="block px-4 py-2 text-gray-700 hover:text-gray-500">login</a> </li>
                         : <>
                             <li>
                                 <a href="#" class="block px-4 py-2 text-gray-700 hover:text-gray-500">profile</a>
