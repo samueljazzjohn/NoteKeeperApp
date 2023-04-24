7d164d1d573f149928f3af46ede7ee37211c2528
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-24 14:48:26 +0530

    set context added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom/client';
 
 import App from "./App"
 import './index.css'
-import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache,ApolloProvider,createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/graphql',
+//   cache: new InMemoryCache()
+// });
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
