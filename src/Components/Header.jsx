ccb8912ad8b6f650fcf314142ede8c2726f16172
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-24 16:02:54 +0530

    removed name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,11 +9,14 @@ function Header({dropDown,setDropdown}) {
   }
   return (
     <header className="sticky top-0 h-[10%] md:h-[8%] bg-[#f5ba13] flex flex-row justify-between items-center px-10 z-20">
-      <h1 className="text-red text-white font-display font-[600] text-[30px]">
+      <h1 className=" text-white font-display font-[600] text-[30px]">
         <HighlightIcon className="-translate-y-1 m-2"/>
         Keeper
       </h1>
+      <div className="flex flex-row justify-center items-center space-x-3">
+        <h2 className="text-white text-[15px] font-semibold">{localStorage.getItem('isLoggedIn')&&localStorage.getItem('username')}</h2>
       <FaUserCircle onClick={handleDropdown} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white cursor-pointer" size={25} />
+        </div>
     </header>
   );
 }
