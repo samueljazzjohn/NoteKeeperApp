03cf6e84a53c795c80a8a4d04defa6e4a20475fa
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-26 16:23:08 +0530

    options dropdown added

---
+++ src/Components/OptionsDropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OptionsDropdown = () => {
+  return (
+    <>
+        <div className='absolute top-10 right-3 border border-gray-300 rounded-md px-2 py-1 w-32 bg-white'>
+            <div className='font-display font-thin cursor-pointer'>list</div>
+        </div>
+    </>
+  )
+}
+
+export default OptionsDropdown
