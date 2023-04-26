c817273ac70fa06b0e6bac859f762ca7bdfc5ac0
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-26 17:11:56 +0530

    naming convention changed

--- src/Components/DropdownMenu.jsx
+++ src/Components/DropdownMenu.jsx
@@ -15,15 +15,15 @@ const DropdownMenu = (props) => {
     }
     return (
         <>
-            {props.DropdownStatus && <div ref={props.refVar} id="dropdown" className="absolute top-3 md:top-8 right-8 z-90 translate-y-10 bg-white divide-y divide-gray-100 rounded-lg  w-44 border border-gray-500 shadow-lg">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    {!localStorage.getItem('isLoggedIn') ? <li><a data-modal-target="defaultModal" data-modal-toggle="defaultModal" onClick={handleLoginModel} href="#" class="block px-4 py-2 text-gray-700 hover:text-gray-500">login</a> </li>
+            {props.DropdownStatus && <div ref={props.refvar} id="dropdown" className="absolute top-3 md:top-8 right-8 z-90 translate-y-10 bg-white divide-y divide-gray-100 rounded-lg  w-44 border border-gray-500 shadow-lg">
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                    {!localStorage.getItem('isLoggedIn') ? <li><a data-modal-target="defaultModal" data-modal-toggle="defaultModal" onClick={handleLoginModel} href="#" className="block px-4 py-2 text-gray-700 hover:text-gray-500">login</a> </li>
                         : <>
                             <li>
-                                <a href="#" class="block px-4 py-2 text-gray-700 hover:text-gray-500">profile</a>
+                                <a href="#" className="block px-4 py-2 text-gray-700 hover:text-gray-500">profile</a>
                             </li>
                             <li>
-                                <a onClick={handleLogout} href="#" class="block px-4 py-2 text-gray-700 hover:text-gray-500">Sign out</a>
+                                <a onClick={handleLogout} href="#" className="block px-4 py-2 text-gray-700 hover:text-gray-500">Sign out</a>
                             </li>
                         </>
                     }
