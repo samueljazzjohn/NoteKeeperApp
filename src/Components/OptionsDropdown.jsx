8e3133d2c4c5bb1742185da050a43bdae640b6f4
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-26 17:12:22 +0530

    bullet list option created

--- src/Components/OptionsDropdown.jsx
+++ src/Components/OptionsDropdown.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const OptionsDropdown = () => {
+const OptionsDropdown = ({toggleListing,setOptionsState}) => {
+
+    const handleClick=()=>{
+        toggleListing()
+        setOptionsState(false)
+    }
   return (
     <>
         <div className='absolute top-10 right-3 border border-gray-300 rounded-md px-2 py-1 w-32 bg-white'>
-            <div className='font-display font-thin cursor-pointer'>list</div>
+            <div onClick={handleClick} className='font-display font-thin cursor-pointer'>list</div>
         </div>
     </>
   )
