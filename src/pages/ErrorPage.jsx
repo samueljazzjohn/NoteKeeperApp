63073e94981a527491505ba4e053d7910b4d08ad
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-28 17:36:01 +0530

    error page designed

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div>404 not found</div>
+    <>
+        <div className='flex h-screen w-full bg-black justify-center items-center'>
+            <h1 className=' text-white font-display' >404 | Not found</h1>
+
+        </div>
+    </>
   )
 }
 
